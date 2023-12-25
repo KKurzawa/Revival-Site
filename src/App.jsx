feef43b555f4f0c5795eebb6eae574fe7b76ec01
Kris Kurzawa <kurzawa1@yahoo.com>
2023-12-25 15:31:44 -0500

    linked pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+// import Footer from './Components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+      <Outlet />
+      {/* <Footer /> */}
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import Home from '../src/Pages/Home/Home.jsx'
+import Shows from '../src/Pages/Shows/Shows.jsx'
+import Band from '../src/Pages/Band/Band.jsx'
+import Testimonials from '../src/Pages/Testimonials/Testimonials.jsx'
+import Contact from '../src/Pages/Contact/Contact.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/Shows',
+        element: <Shows />
+      },
+      {
+        path: '/Shows',
+        element: <Shows />
+      },
+      {
+        path: '/Band',
+        element: <Band />
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: 'Testimonials',
+        element: <Testimonials />,
+      }
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
