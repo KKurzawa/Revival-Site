915dc496f4ad769609b3e9721ad665285a33bd19
Kris Kurzawa <kurzawa1@yahoo.com>
2023-12-26 16:05:51 -0500

    added links to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ import { IoLogoYoutube } from "react-icons/io";
 function Footer() {
     return (
         <div className='flex justify-evenly text-white text-5xl bg-black'>
-            <a href=''><FaFacebook /></a>
-            <a href=''><FaInstagramSquare /></a>
-            <a href=''><IoLogoYoutube /></a>
+            <a href='https://www.facebook.com/revivalmusicdetroit/' target="_blank" rel="noreferrer"><FaFacebook /></a>
+            <a href='https://www.instagram.com/thelucasband/' target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+            <a href='https://www.youtube.com/channel/UCY_f-YAq6RE4hX6qqCcM_3w' target="_blank" rel="noreferrer"><IoLogoYoutube /></a>
         </div>
     )
 }
