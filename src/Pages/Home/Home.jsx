004c2601da7e6629f46af796717d8ff41523cac6
Kris Kurzawa <kurzawa1@yahoo.com>
2023-12-28 21:21:21 -0500

    change mobile text size homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import HomePhoto from '../../assets/Photos/HomePhoto.jpeg';
 
 function Home() {
     return (
-        <main className='flex flex-col justify-center text-2xl md:text-4xl lg:text-5xl'>
+        <main className='flex flex-col justify-center text-xl md:text-4xl lg:text-5xl'>
             <img src={HomePhoto} alt='RevivalPic2' className='' ></img>
             <h2 className='flex justify-center pt-7 pb-1'>The Ultimate Allman Brothers Experience </h2>
             <h3 className='flex justify-center pb-7'>Featuring Detroit's A-List Musicians</h3>
