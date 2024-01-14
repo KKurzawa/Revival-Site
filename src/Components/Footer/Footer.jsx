751d5435406908dac2b12906517209682fe94036
Kris Kurzawa <kurzawa1@yahoo.com>
2024-01-14 16:48:02 -0500

    footer update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,15 @@ import { IoLogoYoutube } from "react-icons/io";
 
 function Footer() {
     return (
-        <div className='flex justify-evenly items-center text-white text-5xl bg-black h-20'>
-            <a href='https://www.facebook.com/revivalmusicdetroit/' target="_blank" rel="noreferrer" className='hover:text-gray-300 transition-all duration-300'><FaFacebook /></a>
-            <a href='https://www.instagram.com/thelucasband/' target="_blank" rel="noreferrer" className='hover:text-gray-300 transition-all duration-300'><FaInstagramSquare /></a>
-            <a href='https://www.youtube.com/channel/UCY_f-YAq6RE4hX6qqCcM_3w' target="_blank" rel="noreferrer" className='hover:text-gray-300 transition-all duration-300'><IoLogoYoutube /></a>
-        </div>
+        <footer className=' bg-black h-40 flex flex-col justify-end'>
+            <div className='flex justify-evenly items-center text-white text-8xl'>
+                <a href='https://www.facebook.com/revivalmusicdetroit/' target="_blank" rel="noreferrer" className='hover:text-gray-300 transition-all duration-300'><FaFacebook /></a>
+                <a href='https://www.instagram.com/thelucasband/' target="_blank" rel="noreferrer" className='hover:text-gray-300 transition-all duration-300'><FaInstagramSquare /></a>
+                <a href='https://www.youtube.com/channel/UCY_f-YAq6RE4hX6qqCcM_3w' target="_blank" rel="noreferrer" className='hover:text-gray-300 transition-all duration-300'><IoLogoYoutube /></a>
+            </div>
+            <p className='text-white'>Site Designed and Built by Kris Kurzawa</p>
+        </footer>
+
     )
 }
 
