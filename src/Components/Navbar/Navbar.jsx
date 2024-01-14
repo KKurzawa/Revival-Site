6479bc6e79895a3ec89d71d05d69c3fdd808fa76
Kris Kurzawa <kurzawa1@yahoo.com>
2024-01-14 15:34:24 -0500

    experiment

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,38 +30,38 @@ function Navbar() {
     // };
 
     return (
-        <div className='bg-black h-36 lg:h-44 w-full flex items-center'>
-            <div className='flex flex-col md:flex-row items-center justify-center md:justify-between h-28'>
-                <div className='flex justify-center items-center'>
-                    <a href='/' className=''>
-                        <img src={Logo} alt='RevivalLogo' className='justify-center w-[75%] md:w-full'></img></a>
+        // <div className='bg-black h-36 lg:h-44 w-full flex items-center'>
+        <div className='bg-black flex flex-col md:flex-row justify-center md:justify-between h-28'>
+            <div className='flex justify-center items-center'>
+                <a href='/' className=''>
+                    <img src={Logo} alt='RevivalLogo' className='justify-center w-[75%] md:w-full'></img></a>
+            </div>
+            <div className=''>
+                <div className='flex items-baseline space-x-4'>
+                    {navLinks.map((link, index) => (
+                        <a key={index} className='text-gray-300 transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-2xl lg:text-4xl font-medium' href={link.link}>{link.title}</a>
+                    ))}
                 </div>
-                <div className=''>
-                    <div className='flex items-baseline space-x-4'>
-                        {navLinks.map((link, index) => (
-                            <a key={index} className='text-gray-300 transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-2xl lg:text-4xl font-medium' href={link.link}>{link.title}</a>
-                        ))}
-                    </div>
-                </div>
-                {/* hamburger menu */}
-                {/* <div className='-m-2 flex flex-col md:hidden'>
+            </div>
+            {/* hamburger menu */}
+            {/* <div className='-m-2 flex flex-col md:hidden'>
                         <button type='button' onClick={handleMenu} className='inline-flex items-center justify-center m-3 rounded-md text-gray-300 text-4xl hover:text-white hover:bg-gray-600 ring-2 ring-offset-2 ring-offset-gray-800 ring-white transition-all duration-300'>
                             <span className='sr-only'>Open Main Menu</span>
                             {open == true ? <FaTimes /> : <FaBars />}
                         </button>
                     </div> */}
 
-            </div>
-            {/* {open ? (
-                <div className='md:hidden h-screen flex flex-col'>
-                    <div className='ox-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                        {navLinks.map((link, index) => (
-                            <a key={index} className='text-gray-300 transition-all duration-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-2xl font-medium text-center' href={link.link}>{link.title}</a>
-                        ))}
-                    </div>
-                </div>
-            ) : null} */}
         </div>
+        /* {open ? (
+            <div className='md:hidden h-screen flex flex-col'>
+                <div className='ox-2 pt-2 pb-3 space-y-1 sm:px-3'>
+                    {navLinks.map((link, index) => (
+                        <a key={index} className='text-gray-300 transition-all duration-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-2xl font-medium text-center' href={link.link}>{link.title}</a>
+                    ))}
+                </div>
+            </div>
+        ) : null}
+    </div> */
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+    },
   },
   plugins: [],
 }
