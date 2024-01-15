122d0762f6fc1cbdb113fbc67e042bd635607cb8
Kris Kurzawa <kurzawa1@yahoo.com>
2024-01-15 17:29:50 -0500

    updated font

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,15 +5,15 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 function Home() {
     return (
         <>
-            <main className='flex lg:hidden flex-col justify-center text-9xl'>
+            <main className='flex lg:hidden flex-col justify-center text-8xl'>
                 <LazyLoadImage src={HomePhoto} alt='RevivalPic2' className='' />
-                <h2 className='flex justify-center mt-20 pt-1 pb-1'>The Ultimate</h2>
+                <h2 className='flex justify-center mt-40 pt-1 pb-1'>The Ultimate</h2>
                 <h2 className='flex justify-center pt-1 pb-1'>Allman Brothers</h2>
                 <h2 className='flex justify-center pt-1 pb-1'>Experience </h2>
-                <h3 className='flex justify-center mt-20 pt-1 pb-1'>Featuring</h3>
+                <h3 className='flex justify-center mt-40 pt-1 pb-1'>Featuring</h3>
                 <h3 className='flex justify-center pt-1 pb-1'>Detroit's</h3>
                 <h3 className='flex justify-center pt-1 pb-1'>A-List </h3>
-                <h3 className='flex justify-center mb-20 pt-1 pb-1'>Musicians</h3>
+                <h3 className='flex justify-center mb-40 pt-1 pb-1'>Musicians</h3>
             </main>
             <main className='hidden lg:flex flex-col justify-center text-7xl'>
                 <LazyLoadImage src={HomePhoto} alt='RevivalPic2' className='' />
