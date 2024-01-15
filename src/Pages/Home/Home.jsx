093dc8909eab051cdbd03fdddc5d9a00a5fc0e5a
Kris Kurzawa <kurzawa1@yahoo.com>
2024-01-15 18:43:14 -0500

    fixed home text

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
                 <h3 className='flex justify-center pt-1 pb-1'>A-List </h3>
                 <h3 className='flex justify-center mb-40 pt-1 pb-1'>Musicians</h3>
             </main>
-            <main className='hidden lg:flex flex-col justify-center text-7xl'>
+            <main className='hidden lg:flex flex-col justify-center text-7xl text-white'>
                 <LazyLoadImage src={HomePhoto} alt='RevivalPic2' className='' />
                 <h2 className='flex justify-center pt-7 pb-1'>The Ultimate Allman Brothers Experience</h2>
                 <h3 className='flex justify-center pb-7'>Featuring Detroit's A-List Musicians</h3>
