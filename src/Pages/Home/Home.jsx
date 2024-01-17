11df882509d9e9cfe62b25daf3eca99bb8912cf3
Kris Kurzawa <kurzawa1@yahoo.com>
2024-01-17 13:30:57 -0500

    linear gradient experiment

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 // import HomePhoto from '../../assets/Photos/HomePhoto.jpeg';
 import Testimonials from '../../assets/Photos/Testimonials.jpeg';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import './Home.css'
 
 function Home() {
     return (
-        <div className='bg-[#130253]'>
+        // <div className='bg-[#130253]'>
+        <div className='main'>
             <main className='flex lg:hidden flex-col justify-center text-8xl text-white'>
                 <LazyLoadImage src={Testimonials} alt='RevivalPic2' className='border-b-8 border-[#c94c2c]' />
                 <h2 className='flex justify-center mt-40 pt-1 pb-1'>The Ultimate</h2>
@@ -16,7 +18,7 @@ function Home() {
                 <h3 className='flex justify-center pt-1 pb-1'>A-List </h3>
                 <h3 className='flex justify-center mb-40 pt-1 pb-1'>Musicians</h3>
             </main>
-            <main className='hidden lg:flex flex-col justify-center text-7xl text-white'>
+            <main className='hidden lg:flex flex-col justify-center text-6xl text-white'>
                 <LazyLoadImage src={Testimonials} alt='RevivalPic2' className='border-b-8 border-[#c94c2c]' />
                 <h2 className='flex justify-center pt-7 pb-1'>The Ultimate Allman Brothers Experience</h2>
                 <h3 className='flex justify-center pb-7'>Featuring Detroit's A-List Musicians</h3>
