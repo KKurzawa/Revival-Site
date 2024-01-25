28a797b96ad062f53c992925f05f91e41ec44ab3
Kris Kurzawa <kurzawa1@yahoo.com>
2024-01-25 16:33:04 -0500

    added ticket link

--- src/Utils/ShowList.jsx
+++ src/Utils/ShowList.jsx
@@ -5,7 +5,7 @@ export const ShowList = [
         time: '8:30pm',
         venue: "The Cadieux Cafe",
         venueLink: 'https://cadieuxcafe.com/',
-        ticketLink: 'https://cadieuxcafe.com/',
+        ticketLink: 'https://www.eventbrite.com/e/revival-a-tribute-to-the-allman-brothers-tickets-809714276197?aff=oddtdtcreator&fbclid=IwAR0Q1wRcP10hT6WPMamtt-73W8FG_1_ThIrN14sPj7XjZHCKbG9ssm4Veds',
     },
     {
         id: 3,
